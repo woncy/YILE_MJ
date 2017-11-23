efdc39a233c124687651d569fa79a4b45c713546
wangxi <13323659953@163.com>
2017-11-23 10:35:08 +0800

    整理client

--- project/mj-client/src/main/java/majiang/client/controller/IndexController.java
+++ project/mj-client/src/main/java/majiang/client/controller/IndexController.java
@@ -20,6 +20,7 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.forkjoin.apikit.core.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
